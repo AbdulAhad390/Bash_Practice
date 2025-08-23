d91c0b09057d0e3a5b7a23d71733f2ef9f5bb4ce
Ahad <a.ahad304567@gmail.com>
2025-08-23 17:14:53 +0500

    first commit

---
+++ hello.sh
@@ -0,0 +1,15 @@
+ #!/bin/bash
+
+ # this is script for 
+
+ echo "hello!"
+
+ name="Ahad"
+ echo "name is $name, and date is $(date)" 
+echo "Enter the name: "
+
+read username
+
+echo "you enter the $username"
+
+echo "the characters in $0 $1 $2 "
