77c3c42ca147810d7a1688a76720611d890cefab
Ahad <a.ahad304567@gmail.com>
2025-08-27 20:03:04 +0500

    Function

---
+++ function.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+ 
+
+   # this is Function 
+    
+
+   function Ahad () {
+   
+   read -p " Enter the num =   " num
+   if  (($num % 2 == 0));
+     then 
+         echo " Even " 
+  else 
+      echo "odd "
+   fi
+}
+ 
+  Ahad 
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
