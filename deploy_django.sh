ed58cf21d2610c747fef2046c338b957639bc255
Ahad <a.ahad304567@gmail.com>
2025-08-29 19:11:30 +0500

    update deploy_django

--- deploy_django.sh
+++ deploy_django.sh
@@ -7,6 +7,23 @@
 Task
 
 
+  code_clone () {
+         
+	  echo " Cloning the Django App " 
+	  git clone 
+  
+  
+  }
 
+  install_requirements() {
+        echo " Installing Dependices " 
+	sudo apt-get install docker.io nginx -y
+  
+  
+  }
 
-
+  required_restarts() {
+        sudo systemctl enable docker
+	sudo systemctl enable nginix
+  
+  }
