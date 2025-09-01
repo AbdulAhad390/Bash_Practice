4961aba7644bd45e6cb3a4f5e96879c755bd546d
Ahad <a.ahad304567@gmail.com>
2025-09-01 17:36:45 +0500

    update deploy_django

--- deploy_django.sh
+++ deploy_django.sh
@@ -10,7 +10,7 @@ Task
   code_clone () {
          
 	  echo " Cloning the Django App " 
-	  git clone 
+	  git clone https://github.com/LondheShubham153/django-notes-app.git
   
   
   }
@@ -23,7 +23,36 @@ Task
   }
 
   required_restarts() {
-        sudo systemctl enable docker
-	sudo systemctl enable nginix
+
+        sudo chown $USER  /var/run/docker.sock 
+	sudo systemctl enable docker
+	sudo systemctl enable nginx
+        sudo  systemctl restart docker
+  }
+
+  deploy() {
+             docker build -t notes-app .
+             docker run -d -p 8000:8000 notes-app:latest
+  
+  
   
   }
+
+  echo " Deploymeny started "
+  if ! code_clone; then 
+	  echo " the code directory already exists "
+	  cd django-notes-app
+  fi
+  if ! install_requirements; then
+	  echo " Installation Failed "
+	  exit 1
+  fi
+  if ! required_restarts; then 
+	  echo "System fault identified "
+	  exit 1 
+  fi
+ if !  deploy; then 
+	 echo "Deployment Failed "
+	 #sendmai
+fi
+  echo " Deployment Done   "
